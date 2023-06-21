1e4cb1dab5095d74a118748f622c000de01f1103
Mark Foskett <markryanfoskett@gmail.com>
2023-06-21 23:51:27 +1000

    added date range picker

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
-import Image from 'next/image'
+import { DateRangePicker } from "@/components/DateRangePicker";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-black">
       <div className='flex flex-col'>
         <h1 className='text-white font-semibold text-2xl'>Travel Planner</h1>
-        <h2 className='text-white font-semibold pt-3'>Choose a starting date...</h2>
-        <h2 className='text-white font-semibold pt-3'>Choose an end date...</h2>
+        <h2 className='text-white font-semibold pt-3'>Choose your trip dates...</h2>
+        <DateRangePicker />
       </div>
     </main>
   )
