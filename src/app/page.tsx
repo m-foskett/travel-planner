cf8cd871da6967cd2621ea271bbc652c591e8172
Mark Foskett <markryanfoskett@gmail.com>
2023-06-22 23:56:23 +1000

    title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-black">
       <div className='flex flex-col'>
-        <h1 className='text-white font-semibold text-2xl'>Travel Planner</h1>
+        <h1 className='text-white font-semibold text-3xl'>Travel Planner</h1>
         <h2 className='text-white font-semibold pt-3'>Choose your trip dates...</h2>
         <DateRangePicker />
       </div>
