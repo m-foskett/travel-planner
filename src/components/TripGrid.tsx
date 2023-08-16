171598a75600e60508def43c5e68989968afe71e
Mark Foskett <markryanfoskett@gmail.com>
2023-08-16 23:17:58 +1000

    added styling

--- src/components/TripGrid.tsx
+++ src/components/TripGrid.tsx
@@ -22,7 +22,7 @@ function TripGrid({dateRange}: TripGridProps) {
         <div key={index} className='text-lg text-white w-40 h-40 border border-solid border-white text-center'>
           Day: {index}
           {/* Dropdown - Actions */}
-          <div className='text-white font-bold text-xs text-center px-3'>
+          <div className='text-white font-bold text-xs text-center px-3 py-3'>
             Actions Dropdown
           </div>
         </div>
