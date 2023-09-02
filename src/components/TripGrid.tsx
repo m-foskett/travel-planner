265c46565ae26ee4fc89c97897ecca898ab8f80f
Mark Foskett <markryanfoskett@gmail.com>
2023-09-02 22:08:15 +1000

    altered grid style

--- src/components/TripGrid.tsx
+++ src/components/TripGrid.tsx
@@ -21,7 +21,7 @@ function TripGrid({ dateRange }: TripGridProps) {
         <div
           key={index}
           className='text-lg text-black bg-white w-40 h-40 border border-solid border-black text-center
-          flex-row flex-wrap items-center'
+          flex-row flex-wrap items-center justify-center'
         >
           Day: {index}
           {/* Dropdown - Actions */}
