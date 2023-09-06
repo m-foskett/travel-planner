d531ec5fa6e801de5b83fe6319ef335bec180af7
Mark Foskett <markryanfoskett@gmail.com>
2023-09-06 21:02:52 +1000

    added dayblock component

--- src/components/TripGrid.tsx
+++ src/components/TripGrid.tsx
@@ -1,6 +1,7 @@
-import { differenceInDays, format } from 'date-fns'
+import { differenceInDays, eachDayOfInterval, format } from 'date-fns'
 import React from 'react'
 import { DateRange } from "react-day-picker"
+import DayBlock from './DayBlock';
 
 interface TripGridProps {
   dateRange: DateRange | undefined,
@@ -9,30 +10,22 @@ interface TripGridProps {
 function TripGrid({ dateRange }: TripGridProps) {
   // Days in the selected trip
   var daysInTrip: number = 2;
+  // All dates within trip
+  var datesInTrip: Date[];
   // Calculate days within date range
   if (dateRange?.from && dateRange.to) {
     daysInTrip = differenceInDays(dateRange.to, dateRange.from) + 1;
+    datesInTrip = eachDayOfInterval({
+      start: dateRange?.from as Date,
+      end: dateRange?.to as Date,
+    });
   }
 
   return (
     // Rendering elements by number of days
     [...Array(daysInTrip),].map(
       (value: undefined, index: number) => (
-        <div
-          key={index}
-          className='text-lg text-black bg-white w-40 h-40 border border-solid border-black text-center
-          flex-row flex-wrap items-center justify-center font-bold italic underline'
-        >
-          Day: {index}
-          {/* Dropdown - Actions */}
-          <div
-            className='text-white font-bold text-xs text-center border border-solid border-white w-20 h-5
-            bg-black hover:opacity-80 hover:cursor-pointer italic underline flex-col top-[10%] left-[74%] translate-x-[-50%]
-            translate-y-[-50%] relative'
-          >
-            Actions
-          </div>
-        </div>
+        <DayBlock key={index} date={datesInTrip[index]} />
       )
     )
   );
